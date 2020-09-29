e3aa2803ba50b8c769f8a921c23e5297e75e35cd
mertgeren <mert.geren@iqb.solutions>
2020-09-29 15:01:36 +0300

    layout definition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+    children: [{        // '' path ile DefaultComponente ulaştıktan sonra burada <router-outlet> kullanılırsa
+      path: '',                           // ve path değişmediyse
+      component: DashboardComponent       // DashboardComponent'in html'ine git
+    }]                  // tabii ki bu children olayının burada kullanılabilmesi için ilgili componentin modülünde (DefaultModule) RouterModule'ün import edilmesi gerekir
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
